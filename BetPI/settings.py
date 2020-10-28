7df671ba87c7f39166407c085aa7f7a16a5ab442
Harry <harrison.yn.kim@gmail.com>
2020-10-27 21:49:31 -0400

    Settings.py Allowed Host to *

--- BetPI/settings.py
+++ BetPI/settings.py
@@ -30,7 +30,7 @@ BET_API_KEY = os.environ['BET_API_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
