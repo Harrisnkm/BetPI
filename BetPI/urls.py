0d238156d22b3e8d63883dbf5b059fd25bbe70e5
Harry <harrison.yn.kim@gmail.com>
2020-10-31 12:39:28 -0400

    redirects root to dashboard and fixes typo in navbar

--- BetPI/urls.py
+++ BetPI/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.redirect_to_dashboard),
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
 ]
